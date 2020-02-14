ae11e3a8ab9f1870ccfe036de25bd39e09adf190
mihir-agarwal0211 <mihiragarwal1999@gmail.com>
2020-02-14 21:47:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/images/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.images;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void Catswitch(View view)
+    {
+        Log.i("Info","Button pressed");
+
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        image.setImageResource(R.drawable.cat1);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
